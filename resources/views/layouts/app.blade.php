9f73cd97e29bcf925d5463b50f7dcf0c8f35ddd9
Zahra Kirschblüte <zahra.kirschblute@hotmail.com>
2020-01-24 16:30:53 +0100

    Create base temaplate for landing #6 -Add babyyoda as 1st npc -Add not available npcs as 2nd and 3rd npc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,10 @@
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href='{{ asset('sass/app.css')}}'>
 </head>
 
 <body>
-<p> This msg is from layout </p> <br>
-
 
 @yield('content')
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
