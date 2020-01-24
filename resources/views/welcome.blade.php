1dea9684af964b6dcc75f187252af9954ae891ca
nbdehlen <nbdehlen@gmail.com>
2020-01-24 17:17:50 +0000

    Fix link in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="container h-100">
     <ul class="row text-center align-items-center h-100 list-unstyled">
         <li class="col-12 col-md-4">
-            <a href="/items"><img src="{{ asset('images/npc-landingpage.png')}}" alt="..." class="img img-thumbnail"></a>
+            <a href="/shop"><img src="{{ asset('images/npc-landingpage.png')}}" alt="..." class="img img-thumbnail"></a>
         </li>
         <li class="col-12 col-md-4">
             <img src="{{ asset('images/npc-landingpage-2.png')}}" alt="..." class="img img-thumbnail notavailable">
