72833318a8c64ed7c222581728ef73ee3ebdb97b
nbdehlen <nbdehlen@gmail.com>
2020-01-24 11:21:08 +0000

    Add live search functionallity with ajax including controller function, route and view with jQuery. Add basic main page representing different vendors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+@yield('head')
+
 </head>
 
 <body>
