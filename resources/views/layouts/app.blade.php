e2a86ad7e0756932b8881ab499ca0e38fb1a9176
nbdehlen <nbdehlen@gmail.com>
2020-01-24 15:19:28 +0000

    Add external js for frontend search and change title #3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Items breh</title>
+<title>Items brollo</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
