a5a7c0fa3214620ff7c09d09fd022a7aa3b6adab
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-01-30 00:18:37 +0100

    Change landing page heaading size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="container d-flex align-items-center h-100">
     <ul class="row text-center align-items-center justify-content-center list-unstyled m-0">
         <div class="col-12 landing-headline">
-            <h1 class="exocet-light display-1 text-uppercase text-white text-shadow mb-0">Traders</h1>
+            <h1 class="exocet-light display-3 text-uppercase text-white text-shadow mb-0">Traders</h1>
             <p class="exocet-light lead text-uppercase text-shadow mb-5">Select your desired trader</p>
         </div>
         <li class="col-12 col-md-4 vendor not-available">
