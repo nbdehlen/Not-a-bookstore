555d014ef5100643fcc020f63dfad774b0ad98ea
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-01-29 19:35:37 +0100

    Improve landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,23 @@
 
 @section('content')
 
-<div class="container h-100">
-
-    <ul class="row text-center align-items-center h-75 list-unstyled m-0">
-        <div class="col-12">
-            <h1 class="exocet-light display-1 text-uppercase text-white">Traders</h1>
-            <p class="lead text-white">Select your desired trader</p>
+<div class="container d-flex align-items-center h-100">
+    <ul class="row text-center align-items-center list-unstyled m-0">
+        <div class="col-12 landing-headline">
+            <h1 class="exocet-light display-1 text-uppercase text-white text-shadow mb-0">Traders</h1>
+            <p class="exocet-light lead text-uppercase text-shadow">Select your desired trader</p>
         </div>
-        <li class="col-12 col-md-4">
-            <img src="{{asset('images/unavailable-vendor-1.png')}}" alt="..." class="img portrait notavailable">
-            <h2 class="exocet h6 text-muted mt-2">Unavailable</h2>
+        <li class="col-12 col-md-4 not-available">
+            <img src="{{asset('images/unavailable-vendor-1.png')}}" alt="..." class="img portrait">
+            <h2 class="exocet h6 text-muted text-shadow mt-2">Unavailable</h2>
         </li>
         <li class="col-12 col-md-4">
             <a href="/shop"><img src="{{ asset('images/main-vendor.png')}}" alt="..." class="img portrait"></a>
-            <h2 class="exocet h6 text-white mt-2">Lilith</h2>
+            <h2 class="exocet h6 text-white text-shadow mt-2">Lilith</h2>
         </li>
-        <li class="col-12 col-md-4">
-            <img src="{{ asset('images/unavailable-vendor-2.png')}}" alt="..." class="img portrait notavailable">
-            <h2 class="exocet h6 text-muted mt-2">Unavailable</h2>
+        <li class="col-12 col-md-4 not-available">
+            <img src="{{ asset('images/unavailable-vendor-2.png')}}" alt="..." class="img portrait">
+            <h2 class="exocet h6 text-muted text-shadow mt-2">Unavailable</h2>
         </li>
 
     </ul>
