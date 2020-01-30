7ff3328dd60e482bb883f47eadd5aba16af123e2
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-01-30 18:01:55 +0100

    Create dialog template #33

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,29 +1,23 @@
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+<div class="modal fade" id="dialog" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+            <div class="modal-header justify-content-center">
+            <h5 class="modal-title exocet">New items in your inventory!</h5>
         </div>
         <div class="modal-body">
-            ...
+            <ul class="exocet-light small">
+                @foreach ($items as $item)
+                    <li>{{ $item->name }} x {{ $item->amount }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="modal-footer justify-content-center">
+            <button type="button" class="btn-accept text-uppercase exocet" data-dismiss="modal">Ok</button>
         </div>
-            <span class="corner tl"></span>
-            <span class="corner tr"></span>
-            <span class="corner bl"></span>
-            <span class="corner br"></span>
+        <span class="corner tl"></span>
+        <span class="corner tr"></span>
+        <span class="corner bl"></span>
+        <span class="corner br"></span>
         </div>
     </div>
 </div>
