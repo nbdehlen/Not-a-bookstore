d2c6990376272fe38d471e13974647d65216121c
nbdehlen <nbdehlen@gmail.com>
2020-01-30 15:22:59 +0000

    merge master with dynamic search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,5 +1,5 @@
 <div class="col p-0">
-    <input type="search" class="form-control">
+    <input type="search" class="form-control" id="search" name="search">
 </div>
 <div class="col-auto p-0">
     <button class="btn btn-texture-red exocet">Search</button>
