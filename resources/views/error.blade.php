b9a18fdbe8fde0e48eea4a70ed679e7e33206e69
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-02-13 10:20:00 +0100

    Fix error template

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -5,7 +5,7 @@
     <div class="row h-100">
         <div class="col-auto mx-auto my-auto">
             <img src="https://http.cat/{{ $code }}" alt="Error {{ $code }}">
-            <p class="lead text-center text-white mt-2">{{ $message }}</p>
+            <p class="lead text-center text-white mt-2">{{ $message ?? '' }}</p>
         </div>
     </div>
 </div>
